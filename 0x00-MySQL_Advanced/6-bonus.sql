053b14ae885a3c52e28f1b76a85a825fba891236
Ahmed <ahmed.mekkawi.74@gmail.com>
2024-04-26 05:31:16 +0300

    MySQL_Advanced

---
+++ 0x00-MySQL_Advanced/6-bonus.sql
@@ -0,0 +1,11 @@
+-- mysql script
+DELIMITER //
+CREATE PROCEDURE AddBonus(IN user_id INT, IN project_name VARCHAR(225), IN score INT)
+BEGIN
+IF project_name IS NULL THEN
+    INSERT INTO projects (name) VALUES (project_name);
+END IF;
+INSERT INTO corrections (user_id, project_id, score) VALUES (user_id, (SELECT id FROM projects WHERE name = project_name), score);
+END //
+
+DELIMITER ;
