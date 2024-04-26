9fcecec7389d69febc16a56098d0f6795f27f712
Ahmed <ahmed.mekkawi.74@gmail.com>
2024-04-26 03:54:29 +0300

    MySQL_Advanced

--- 0x00-MySQL_Advanced/5-valid_email.sql
+++ 0x00-MySQL_Advanced/5-valid_email.sql
@@ -1,7 +1,9 @@
 -- mysql script
+DELIMITER //
 CREATE TRIGGER users_email_trigger
 BEFORE UPDATE ON users
 FOR EACH ROW
 IF OLD.email <> NEW.email THEN
 SET NEW.valid_email = 0;
-END IF;
+END IF//
+DELIMITER ;
