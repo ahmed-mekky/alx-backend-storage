89d5a94c7970a1414953923b3897b3c090e1d48c
Ahmed <ahmed.mekkawi.74@gmail.com>
2024-04-28 19:56:24 +0300

    MySQL_Advanced

--- 0x00-MySQL_Advanced/100-average_weighted_score.sql
+++ 0x00-MySQL_Advanced/100-average_weighted_score.sql
@@ -1,9 +1,9 @@
 -- mysql script
 DELIMITER //
-CREATE PROCEDURE ComputeAverageScoreForUser(IN userId INT)
+CREATE PROCEDURE ComputeAverageWeightedScoreForUser(IN userId INT)
 BEGIN
 DECLARE average FLOAT;
-SELECT (SELECT sum(score) FROM corrections WHERE user_id = userId) / (SELECT SUM(weight) FROM projects WHERE id IN (select project_id from corrections where user_id = userId)) INTO average
+SELECT SUM(score * (SELECT weight FROM projects WHERE id = project_id)) / (SELECT sum(weight) FROM projects) INTO average FROM corrections WHERE user_id = userId;
 
 UPDATE users
 SET average_score = average
--- 0x00-MySQL_Advanced/101-average_weighted_score.sql
+++ 0x00-MySQL_Advanced/101-average_weighted_score.sql
@@ -1,13 +1,28 @@
 -- mysql script
 DELIMITER //
-CREATE PROCEDURE ComputeAverageScoreForUser(IN userId INT)
+CREATE PROCEDURE ComputeAverageWeightedScoreForUsers()
 BEGIN
 DECLARE average FLOAT;
-SELECT sum(score) / count(score) INTO average FROM corrections WHERE user_id = userId;
+DECLARE user_id INT;
 
-UPDATE users
-SET average_score = average
-WHERE id = userId;
-END //
+DECLARE total_weight FLOAT;
+
+SELECT SUM(weight) INTO total_weight FROM projects;
+
+SELECT id INTO user_id FROM users;
+
+WHILE user_id IS NOT NULL DO
+  SELECT SUM(score * weight) / total_weight INTO average
+  FROM corrections
+  WHERE user_id = user_id;
+
+  UPDATE users
+  SET average_score = average
+  WHERE id = user_id;
+
+  SELECT id INTO user_id FROM users
+  WHERE id > user_id;
+END WHILE;
+END//
 
 DELIMITER ;
