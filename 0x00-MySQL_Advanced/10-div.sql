4932d099511774743ccffdb9f439da7656d06228
Ahmed <ahmed.mekkawi.74@gmail.com>
2024-04-28 05:45:03 +0300

    MySQL_Advanced

--- 0x00-MySQL_Advanced/10-div.sql
+++ 0x00-MySQL_Advanced/10-div.sql
@@ -5,12 +5,10 @@ CREATE FUNCTION SafeDiv(a INT, b INT)
 RETURNS FLOAT
 DETERMINISTIC
 BEGIN
-DECLARE div_value FLOAT;
 IF b = 0 THEN
-SET div_value = 0;
+RETURN 0;
 ELSE
-SET div_value = a / b;
+RETURN a / b;
 END IF;
-RETURN div_value;
-END
+END//
 DELIMITER ;
