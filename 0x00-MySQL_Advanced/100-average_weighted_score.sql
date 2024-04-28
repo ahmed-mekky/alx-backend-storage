ce914312539bf832813a470a8b58dad232f2a4af
Ahmed <ahmed.mekkawi.74@gmail.com>
2024-04-28 05:59:18 +0300

    MySQL_Advanced

--- 0x00-MySQL_Advanced/100-average_weighted_score.sql
+++ 0x00-MySQL_Advanced/100-average_weighted_score.sql
@@ -3,7 +3,7 @@ DELIMITER //
 CREATE PROCEDURE ComputeAverageScoreForUser(IN userId INT)
 BEGIN
 DECLARE average FLOAT;
-SELECT sum(score) / count(score) INTO average FROM corrections WHERE user_id = userId;
+SELECT (SELECT sum(score) FROM corrections WHERE user_id = userId) / (SELECT SUM(weight) FROM projects WHERE id IN (select project_id from corrections where user_id = userId)) INTO average
 
 UPDATE users
 SET average_score = average
