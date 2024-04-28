ac16b1c199b7852409e2c8412dd64e8d9edf94ce
Ahmed <ahmed.mekkawi.74@gmail.com>
2024-04-28 04:10:03 +0300

    MySQL_Advanced

--- 0x00-MySQL_Advanced/7-average_score.sql
+++ 0x00-MySQL_Advanced/7-average_score.sql
@@ -1,8 +1,8 @@
 -- mysql script
 DELIMITER //
-CREATE PROCEDURE ComputeAverageScoreForUser(in userId int)
+CREATE PROCEDURE ComputeAverageScoreForUser(IN userId INT)
 BEGIN
-DECLARE average int;
+DECLARE average INT;
 SELECT sum(score) / count(score) INTO average FROM corrections WHERE user_id = userId;
 UPDATE users
 SET average_score = average
