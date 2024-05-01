28f98cbdb8124a7377d6a57f05134a71e5ec5f0d
Ahmed <ahmed.mekkawi.74@gmail.com>
2024-05-01 05:53:23 +0300

    NoSQL

---
+++ 0x01-NoSQL/12-log_stats.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python3
+"""python script for mongodb"""
+
+
+from pymongo import MongoClient
+
+client = MongoClient('localhost', 27017)
+coll = client.logs.nginx
+
+print(f"{coll.count_documents({})} logs")
+print("Methods:")
+methods = ["GET", "POST", "PUT", "PATCH", "DELETE"]
+for method in methods:
+    print(f"\tmethod {method}: {coll.count_documents({'method': method})}")
+print(f"{coll.count_documents({{method: 'GET'}, {path: '/status'}})} status check")
