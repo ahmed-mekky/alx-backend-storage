28378185d5a1b605e0b02555d71f21e5fd412837
Ahmed <ahmed.mekkawi.74@gmail.com>
2024-05-03 13:10:33 +0300

    redis

--- 0x02-redis_basic/exercise.py
+++ 0x02-redis_basic/exercise.py
@@ -8,7 +8,8 @@ class Cache:
     """Cache class"""
     def __init__(self):
         """Constructor"""
-        self._redis = redis.Redis().flushdb()
+        self._redis = redis.Redis()
+        self._redis.flushdb()
 
     def store(self, data: Union[str, bytes, int, float]) -> str:
         """
