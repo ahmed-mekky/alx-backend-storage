310a947004f7984b2ae2ee4a2538c9ae2407054d
Ahmed <ahmed.mekkawi.74@gmail.com>
2024-05-03 12:05:23 +0300

    redis

--- 0x02-redis_basic/exercise.py
+++ 0x02-redis_basic/exercise.py
@@ -1,13 +1,24 @@
 #!/usr/bin/env python3
+"""
+Cache module
+"""
 import redis
 from uuid import uuid4
 from typing import Union
 
 class Cache:
+    """
+    Cache class
+    """
     def __init__(self):
+        """
+        Constructor"""
         self ._redis = redis.Redis()
 
     def store(self, data: Union[int, str, bytes, float]) -> str:
+        """
+        Method that takes a data argument and returns a string
+        """
         key = str(uuid4())
         self._redis.set(key, data)
         return key
